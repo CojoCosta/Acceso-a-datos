b95693356a981fd72c3fe3e4e66d2cc6b41a5b8a
cojocosta <diegocostapereira18@gmail.com>
2025-09-22 18:26:41 +0200

    changes

---
+++ Primer Tema/Ejercicio9.java
@@ -0,0 +1,42 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+        do {
+            System.out.println("1.- Dar de alta alumnos");
+            System.out.println("2.- Consultar alumnos");
+            System.out.println("3.- Modificar alumnos");
+            System.out.println("4.- Borrar alumnos");
+            System.out.println("5.- Salir");
+            try {
+                option = sc.nextInt();
+            } catch (InputMismatchException e) {
+            }
+            sc.nextLine();
+            switch (option) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    System.out.println("Saliste");
+                    break;
+            
+                default:
+                System.out.println("Del 1 al 5");
+                    break;
+            }
+        } while (option != 5);
+    }
+}
