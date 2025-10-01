283347fa7d3a8af0d0683567d7bee0cc17428424
Diego Costa <diegocostapereira18@gmail.com>
2025-10-01 13:02:54 +0200

    changes

---
+++ SegundoTema/creacionDm.java
@@ -0,0 +1,25 @@
+package SegundoTema;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+
+public class creacionDm {
+    public static Document creaArbol(String ruta) {
+        Document doc = null;
+        try {
+            DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
+            factoria.setIgnoringComments(true);
+            DocumentBuilder builder = factoria.newDocumentBuilder();
+            doc = builder.parse(ruta);
+        } catch (Exception e) {
+            System.out.println("Error generando el árbol DOM: " + e.getMessage());
+        }
+        return doc;
+    }
+
+    public static void main(String[] args) {
+        String ruta = "achivo.xml";
+        Document doc = creaArbol(ruta);
+    }
+}
