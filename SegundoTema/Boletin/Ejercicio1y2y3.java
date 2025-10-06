30bd2771c4a0e6810e13900cbe5630d0d1b9e083
Diego Costa <diegocostapereira18@gmail.com>
2025-10-06 18:12:00 +0200

    changes

--- SegundoTema/Boletin/Ejercicio1y2y3.java
+++ SegundoTema/Boletin/Ejercicio1y2y3.java
@@ -4,10 +4,11 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class Ejercicio1y2 {
+public class Ejercicio1y2y3 {
     public static Document creaArbol(String ruta) {
         Document doc = null;
         try {
@@ -43,10 +44,19 @@ public class Ejercicio1y2 {
     public static void mostrarTodo(Document doc) {
         Node filmoteca, peliculaNode, tituloDirector, director;
         NodeList pelicula, hijosPelicula, directores;
+        NamedNodeMap atributos;
         filmoteca = doc.getFirstChild();
         pelicula = filmoteca.getChildNodes();
         for (int i = 0; i < pelicula.getLength(); i++) {
             peliculaNode = pelicula.item(i);
+            if (peliculaNode.hasAttributes()) {
+                atributos = peliculaNode.getAttributes();
+                for (int j2 = 0; j2 < atributos.getLength(); j2++) {
+                    if (atributos.item(j2).getNodeName() == "genero") {
+                        System.out.printf("%S: %s\n", atributos.item(j2).getNodeName(), atributos.item(j2).getTextContent());
+                    }
+                }
+            }
             if (peliculaNode.getNodeType() == Node.ELEMENT_NODE) {
                 hijosPelicula = peliculaNode.getChildNodes();
                 for (int j = 0; j < hijosPelicula.getLength(); j++) {
@@ -54,7 +64,12 @@ public class Ejercicio1y2 {
                     if (tituloDirector.getNodeType() == Node.ELEMENT_NODE) {
                         if (tituloDirector.getNodeName() == "director") {
                             directores = tituloDirector.getChildNodes();
-                            System.out.printf("%S: %s %s ",tituloDirector.getNodeName(), directores.item(j).getTextContent(), directores.item(j).getTextContent());
+                            for (int k = 0; k < directores.getLength(); k++) {
+                                director = directores.item(k);
+                                if (director.getNodeType() == Node.ELEMENT_NODE) {
+                                    System.out.printf("%S: %s ", directores.item(k).getNodeName(), directores.item(k).getTextContent());
+                                }
+                            }
                         } else {
                             System.out.printf("%S: %s ", tituloDirector.getNodeName(), tituloDirector.getTextContent());
                         }
@@ -69,9 +84,10 @@ public class Ejercicio1y2 {
         String ruta = "Boletin\\Peliculas.xml";
         Document doc = creaArbol(ruta);
 
-        // mostrarTitulos(doc);
+        mostrarTitulos(doc);
 
         mostrarTodo(doc);
 
+        
     }
 }
