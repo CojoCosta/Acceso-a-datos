262c689df127f711f97c2e3bef07fbad8cc26d6a
CojoCosta <dcosta1882000@gmail.com>
2025-10-13 18:04:22 +0200

    changes

--- SegundoTema/Boletin/EjercicioBoletin.java
+++ SegundoTema/Boletin/EjercicioBoletin.java
@@ -1,5 +1,7 @@
 package Boletin;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -10,6 +12,10 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSSerializer;
 
 public class EjercicioBoletin {
     public static Document creaArbol(String ruta) {
@@ -131,30 +137,97 @@ public class EjercicioBoletin {
         System.out.printf("Nº DE GENEROS DIFRENTES %d", generos.size());
     }
 
-    public static void añadirAtributo(Document doc, String titulo, String atributo) {
-        Node filmoteca;
-        NodeList pelicula, titulos;
-        Element peliculaNode;
-        NamedNodeMap atributos;
-        filmoteca = doc.getFirstChild();
-        pelicula = filmoteca.getChildNodes();
-        for (int i = 0; i < pelicula.getLength(); i++) {
-            if (pelicula.item(i).getNodeType() == Node.ELEMENT_NODE) {
-                peliculaNode = (Element) pelicula.item(i);
-                titulos = peliculaNode.getElementsByTagName("titulo");
-                if (titulos.item(i).getTextContent() == titulo) {
-                    atributos = peliculaNode.getAttributes();
-                    for (int j = 0; j < atributos.getLength(); j++) {
-                        if (atributos.item(j).getNodeName() != atributo) {
-                            peliculaNode.setAttribute(atributo, "150 mins");
-                        }
+    public static void añadirAtributo(Document doc, String tituloPelicula, String atributo) {
+        NodeList peliculas = doc.getElementsByTagName("pelicula");
+        for (int i = 0; i < peliculas.getLength(); i++) {
+            Element pelicula = (Element) peliculas.item(i);
+            String titulo = pelicula.getElementsByTagName("titulo").item(0).getTextContent();
+            if (titulo.equalsIgnoreCase(tituloPelicula) && !pelicula.hasAttribute(atributo)) {
+                pelicula.setAttribute("version", atributo);
+                System.out.println("Atributo añadido");
+            }
+        }
+    }
+
+    public static void eliminarAtributo(Document doc, String tituloPelicula, String atributo) {
+        NodeList peliculas = doc.getElementsByTagName("pelicula");
+        for (int i = 0; i < peliculas.getLength(); i++) {
+            Element pelicula = (Element) peliculas.item(i);
+            String titulo = pelicula.getElementsByTagName("titulo").item(0).getTextContent();
+            if (titulo.equalsIgnoreCase(tituloPelicula) && pelicula.hasAttribute(atributo)) {
+                pelicula.removeAttribute(atributo);
+                System.out.println(atributo + "eliminado");
+            }
+        }
+    }
+    public static void grabarDOM(Document document, String ficheroSalida)
+            throws ClassNotFoundException, InstantiationException,
+            IllegalAccessException, FileNotFoundException {
+        DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+        DOMImplementationLS ls = (DOMImplementationLS) registry.getDOMImplementation("XML 3.0 LS 3.0");
+        // Se crea un destino vacio
+        LSOutput output = ls.createLSOutput();
+        output.setEncoding("UTF-8");
+        // Se establece el flujo de salida
+        output.setByteStream(new FileOutputStream(ficheroSalida));
+        // output.setByteStream(System.out);
+        // Permite escribir un documento DOM en XML
+        LSSerializer serializer = ls.createLSSerializer();
+        // Se establecen las propiedades del serializador
+        serializer.setNewLine("\r\n");
+        ;
+        serializer.getDomConfig().setParameter("format-pretty-print", true);
+        // Se escribe el documento ya sea en un fichero o en una cadena de texto
+        serializer.write(document, output);
+        // String xmlCad=serializer.writeToString(document);
+    }
+
+    public static void añadirPelicula(Document doc, String titulo, String nombreAutor, String apellidoAutor,String año, String genero, String idioma){
+        Element nodoPelicula = doc.createElement("pelicula");
+        nodoPelicula.setAttribute("año",año);
+        nodoPelicula.setAttribute("genero", genero);
+        nodoPelicula.setAttribute("idioma", idioma);
+        nodoPelicula.appendChild(doc.createTextNode("\n"));
+
+        Element nodoTitulo = doc.createElement("titulo");
+        nodoTitulo.appendChild(doc.createTextNode(titulo));
+        nodoPelicula.appendChild(nodoTitulo);
+        nodoTitulo.appendChild(doc.createTextNode("\n"));
+        
+        Element nodoDirector = doc.createElement("director");
+        Node nombre = nodoDirector.appendChild(doc.createElement("nombre"));
+        nombre.appendChild(doc.createTextNode(nombreAutor));
+        Node apellido = nodoDirector.appendChild(doc.createElement("apellido"));
+        apellido.appendChild(doc.createTextNode(apellidoAutor));
+        nodoPelicula.appendChild(nodoDirector);
+        nodoDirector.appendChild(doc.createTextNode("\n"));
+        
+        Node raiz = doc.getFirstChild();
+        raiz.appendChild(nodoPelicula);
+        raiz.appendChild(doc.createTextNode("\n"));
+    }
+    public static void modificarDirector(Document doc, String nombreNuevo, String nombreViejo, String ape){
+        NodeList directores = doc.getElementsByTagName("director");
+        for (int i = 0; i < directores.getLength(); i++) {
+            Node director = directores.item(i);
+            if (director.getFirstChild().getNodeType() != Node.ELEMENT_NODE) {
+                Node nombre = director.getFirstChild().getNextSibling();
+                NodeList apellidos = doc.getElementsByTagName("apellido");
+                for (int j = 0; j < apellidos.getLength(); j++) {
+                    if (nombre.getTextContent().equals(nombreViejo) && apellidos.item(j).getTextContent().equals(ape)) {
+                        System.out.println(nombre.getTextContent());
+                        System.out.println(apellidos.item(j).getTextContent());
+                        nombre.setTextContent(nombreNuevo);
                     }
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static void añadirAlfredo(Document doc, String titulo){
+        
+    }
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, FileNotFoundException {
         String ruta = "Boletin\\Peliculas.xml";
         Document doc = creaArbol(ruta);
         // System.out.println("Ejercicio 2");
@@ -166,8 +239,15 @@ public class EjercicioBoletin {
         // contarDirectores(doc, 1);
         // System.out.println("Ejercicio 6");
         // diferentesGeneros(doc);
-        System.out.println("Ejercicio 7");
-        añadirAtributo(doc, "Dune", "duracion");
+        // System.out.println("Ejercicio 7");
+        // añadirAtributo(doc, "Dune", "Contenido");
+        // eliminarAtributo(doc, "Dune", "version");
+        // System.out.println("Ejercicio 8");
+        // añadirPelicula(doc, "Depredador", "John", "Tiernan", "1987", "acción", "vo");
+        // grabarDOM(doc, ruta);
+        // System.out.println("Ejercicio 9");
+        // modificarDirector(doc, "Lana", "Larry", "Wachowski");
+        // grabarDOM(doc, ruta);
 
     }
 }
