ca95a340faa46f3efa7aac88e2abdb6b8cec454c
CojoCosta <dcosta1882000@gmail.com>
2025-10-23 13:57:45 +0200

    changes

--- tema3/src/main/java/json/Application.java
+++ tema3/src/main/java/json/Application.java
@@ -2,6 +2,7 @@ package json;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +15,7 @@ import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonValue;
 import javax.json.JsonWriter;
+import javax.json.stream.JsonGenerator;
 import javax.net.ssl.HttpsURLConnection;
 
 public class Application {
@@ -96,7 +98,8 @@ public class Application {
       for (JsonValue interprete : interpretes) {
         JsonObject inter = interprete.asJsonObject();
         System.out.printf("+ Nombre: %s \n", inter.getString("nombre"));
-        System.out.printf("Año: %d \nMes: %d\n", inter.getJsonObject("fechaNacimiento").getInt("año"), inter.getJsonObject("fechaNacimiento").getInt("mes"));
+        System.out.printf("Año: %d \nMes: %d\n", inter.getJsonObject("fechaNacimiento").getInt("año"),
+            inter.getJsonObject("fechaNacimiento").getInt("mes"));
       }
     }
   }
@@ -137,10 +140,39 @@ public class Application {
     return array;
   }
 
+  public static void generaEndisco(File f) throws FileNotFoundException {
+    JsonGenerator generator = Json.createGenerator(new FileOutputStream(f));
+    generator.writeStartArray()
+        .writeStartObject()
+        .write("titulo", "El atlas de las nubes")
+        .write("año", 2012)
+        .write("directores", "Lana Wachowski, Lilly Wachowski")
+        .writeStartArray("intepretes")
+        .writeStartObject()
+        .write("nombre", "Tom Hanks")
+        .writeStartObject("fechaNacimiento")
+        .write("año", "1956")
+        .write("mes", 8)
+        .writeEnd()
+        .writeEnd()
+        .writeStartObject()
+        .write("nombre", "Halle Berry")
+        .writeStartObject("fechaNacimiento")
+        .write("año", "1966")
+        .write("mes", 7)
+        .writeEnd()
+        .writeEnd()
+        .writeEnd()
+        .writeEnd()
+        .writeEnd()
+        .close();
+  }
+
   public static void main(String[] args) throws FileNotFoundException {
     // System.out.println(leeJSON("https://native-stats.org/competition/PD/"));
     // JsonValue json = leeJSON("https://pokeapi.co/api/v2/pokemon/ditto");
-    // escribeJSON(json, new File("src\\main\\java\\Resources\\ditto.json"));
-    navegarPelis();
+    // escribeJSON(json, new File("src\main\java\Resources\ditto.json"));
+    // navegarPelis();
+    generaEndisco(new File("src\\main\\java\\Resources\\pelisgenerado.json"));
   }
 }
