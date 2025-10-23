f075afaf6a672f54df549b4782ff1466f6237214
CojoCosta <dcosta1882000@gmail.com>
2025-10-23 13:42:19 +0200

    changes

--- tema3/src/main/java/json/Application.java
+++ tema3/src/main/java/json/Application.java
@@ -1,14 +1,19 @@
 package json;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.URL;
 
 import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonValue;
+import javax.json.JsonWriter;
 import javax.net.ssl.HttpsURLConnection;
 
 public class Application {
@@ -61,7 +66,43 @@ public class Application {
     }
   }
 
-  public static void main(String[] args) {
-    System.out.println(leeJSON("https://native-stats.org/competition/PD/"));
+  public static void escribeJSON(JsonValue json, File f) throws FileNotFoundException {
+    System.out.println("Guardando tipo: " + json.getValueType());
+    PrintWriter pw = new PrintWriter(f);
+    JsonWriter writer = Json.createWriter(pw);
+    // writer.write((JsonStructure) json);
+    if (json.getValueType() == JsonValue.ValueType.OBJECT) {
+      writer.writeObject(json.asJsonObject());
+      // writer.writeObject((JsonObject)json);
+    } else if (json.getValueType() == JsonValue.ValueType.ARRAY) {
+      writer.writeArray(json.asJsonArray());
+      // writer.writeArray((JsonArray)json);
+    } else
+      System.out.println("No se soporta la escritura");
+      writer.close();
+  }
+  public static void navegarPelis(){
+    JsonValue jv = leeJSON("src\\main\\java\\Resources\\pelis.json");
+    System.out.println(jv);
+    JsonArray ja = jv.asJsonArray();
+    for (JsonValue peli : ja) {
+      JsonObject p = peli.asJsonObject();
+      System.out.printf("+ Titulo: %s \n- Año: %d\n",p.getString("titulo"), p.getInt("año"));
+      JsonArray interpretes = p.getJsonArray("interpretes");
+      System.out.println("Interpretes");
+      for (JsonValue interprete : interpretes) {
+        JsonObject inter = interprete.asJsonObject();
+        System.out.printf("+ Nombre: %s \n",inter.getString("nombre"));
+        System.out.printf("Año: %d \nMes: %d\n", inter.getJsonObject("fechaNacimiento").getInt("año"), inter.getJsonObject("fechaNacimiento").getInt("mes"));
+      }
+
+    }
+  }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    // System.out.println(leeJSON("https://native-stats.org/competition/PD/"));
+    // JsonValue json = leeJSON("https://pokeapi.co/api/v2/pokemon/ditto");
+    // escribeJSON(json, new File("src\\main\\java\\Resources\\ditto.json"));
+    navegarPelis();
   }
 }
