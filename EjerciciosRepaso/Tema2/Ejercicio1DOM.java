261815a33f8fb2b35c31bff521765259b469e32d
CojoCosta <dcosta1882000@gmail.com>
2025-11-18 10:46:28 +0100

    changes18/11

--- EjerciciosRepaso/Tema2/Ejercicio1DOM.java
+++ EjerciciosRepaso/Tema2/Ejercicio1DOM.java
@@ -1,9 +1,13 @@
 package EjerciciosRepaso.Tema2;
 
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class Ejercicio1DOM {
     public static Document creaArbol(String ruta) {
@@ -18,7 +22,44 @@ public class Ejercicio1DOM {
         }
         return doc;
     }
+
     public static void main(String[] args) {
-        
+        Document doc = creaArbol("EjerciciosRepaso\\Tema2\\liga.xml");
+        NodeList temporada;        
+        temporada = doc.getElementsByTagName("temporada");
+        for (int i = 0; i < temporada.getLength(); i++) {
+            System.out.printf("Temporada: %s\n", temporada.item(i).getTextContent());
+        }
+
+        System.out.println("----------------EJ2----------------------");
+        NodeList evento;
+        evento = doc.getElementsByTagName("evento");
+        System.out.printf("Nº partidos: %d\n",evento.getLength());
+
+
+        System.out.println("------------------EJ3-------------------");
+        NodeList evento3;
+        evento3 = doc.getElementsByTagName("evento");
+        for (int i = 0; i < evento3.getLength(); i++) {
+            if (evento3.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                Element cadaEvento = (Element)evento3.item(i);
+                String fecha = cadaEvento.getElementsByTagName("fecha").item(0).getTextContent();
+                String local = cadaEvento.getElementsByTagName("equipolocal").item(0).getTextContent();
+                String visitante = cadaEvento.getElementsByTagName("equipovisitante").item(0).getTextContent();
+                System.out.printf("Fecha: %s, Local %15s, Visitante: %15s\n",fecha,local,visitante);
+            }
+        }
+
+        System.out.println("------------------EJ4-------------------");
+        NodeList equipo = doc.getElementsByTagName("team");
+        int maxGoles = 0;
+        for (int i = 0; i < equipo.getLength(); i++) {
+            if (equipo.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                Element team = (Element)equipo.item(i);
+                String goles = team.getElementsByTagName("goals_scored").item(0).getTextContent();
+                int goals = Integer.parseInt(goles);
+
+            }
+        }
     }
 }
--- EjerciciosRepaso/Tema2/Ejercicio1SAX.java
+++ EjerciciosRepaso/Tema2/Ejercicio1SAX.java
@@ -8,31 +8,26 @@ public class Ejercicio1SAX extends DefaultHandler{
 
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
-        // TODO Auto-generated method stub
         super.characters(ch, start, length);
     }
 
     @Override
     public void startDocument() throws SAXException {
-        // TODO Auto-generated method stub
         super.startDocument();
     }
-    
+
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        // TODO Auto-generated method stub
         super.startElement(uri, localName, qName, attributes);
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
-        // TODO Auto-generated method stub
         super.endElement(uri, localName, qName);
     }
 
     @Override
     public void endDocument() throws SAXException {
-        // TODO Auto-generated method stub
         super.endDocument();
     }
 }
