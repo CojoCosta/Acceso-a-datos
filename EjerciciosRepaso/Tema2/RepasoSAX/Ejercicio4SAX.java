958182a0fc98c30d1619d3e9cb9bc3e9f94093c2
CojoCosta <dcosta1882000@gmail.com>
2025-11-18 13:35:39 +0100

    changes18/11

--- EjerciciosRepaso/Tema2/RepasoSAX/Ejercicio4SAX.java
+++ EjerciciosRepaso/Tema2/RepasoSAX/Ejercicio4SAX.java
@@ -7,34 +7,42 @@ import org.xml.sax.helpers.DefaultHandler;
 public class Ejercicio4SAX extends DefaultHandler {
     int maxGoles = 0;
     String cantidadGoles;
-    String nombreEquipo;
-    boolean flag = false;
+    String equipo;
+    String equipoGoleador;
+    boolean esGol = false;
+    boolean esEquipo = false;
 
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
         super.characters(ch, start, length);
         cantidadGoles = new String(ch, start, length);
-        nombreEquipo = new String(ch, start, length);
-        if (flag) {
+        if (esGol) {
             if (Integer.parseInt(cantidadGoles) > maxGoles) {
                 maxGoles = Integer.parseInt(cantidadGoles);
             }
-            flag = false;
+            esGol = false;
         }
-    }
-
-    @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        super.startElement(uri, localName, qName, attributes);
-        if (qName == "goals_scored") {
-            flag = true;
+        equipo = new String(ch, start, length);
+        if (esEquipo) {
+            equipoGoleador = equipo;
+            esEquipo = false;
         }
-        
     }
 
     @Override
     public void startDocument() throws SAXException {
         super.startDocument();
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        if (qName == "name") {
+            esEquipo = true;
+        }
+        if (qName == "goals_scored") {
+            esGol = true;
+        }
     }
 
     @Override
@@ -46,7 +54,6 @@ public class Ejercicio4SAX extends DefaultHandler {
     @Override
     public void endDocument() throws SAXException {
         super.endDocument();
-        System.out.println(maxGoles);
+        System.out.println(equipoGoleador + maxGoles);
     }
-
 }
