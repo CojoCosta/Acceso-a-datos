419e5c01c1ebe488273ed0657c69bc90d4c96846
CojoCosta <dcosta1882000@gmail.com>
2025-12-01 18:18:22 +0100

    changes 1/12

--- Tema 4/Boletin/src/ejercicios.java
+++ Tema 4/Boletin/src/ejercicios.java
@@ -34,30 +34,24 @@ public class ejercicios {
         }
     }
 
-    public static void consultarDatos(String a){
-        try (Statement st = conexion.createStatement()){
+    public static void consultarDatos(String a) {
+        try (Statement st = conexion.createStatement()) {
             String consulta = String.format("SELECT * FROM alumnos Where nombre LIKE '%%%s%%'", a);
             ResultSet resultadoConsulta = st.executeQuery(consulta);
             while (resultadoConsulta.next()) {
-                System.out.printf("%d \t %s\n",resultadoConsulta.getInt("codigo"), resultadoConsulta.getString("nombre") );
+                System.out.printf("%d \t %s\n", resultadoConsulta.getInt("codigo"),
+                        resultadoConsulta.getString("nombre"));
             }
         } catch (SQLException e) {
             System.out.println("ERROR");
         }
     }
 
-    public static void altaAlumno(String nombre, String apellidos, int altura, int aula){
+    public static void altaAlumno(String nombre, String apellidos, int altura, int aula) {
         try (Statement st = conexion.createStatement()) {
-            String consulta = String.format("INSERT INTO jugadores_celta (nombre, apellidos, altura, aula) Values (%s, %s, %d, %d)",nombre, apellidos,altura, aula);
-            int numFilasAfectadas = st.executeUpdate(consulta);
-            System.out.println(numFilasAfectadas);
-        } catch (SQLException e) {
-            System.out.println("ERROR");
-        }
-    }
-    public static void altaAsignatura(String nombre){
-        try (Statement st = conexion.createStatement()) {
-            String consulta = String.format("INSERT INTO jugadores_celta (nombre) Values (%s)",nombre);
+            String consulta = String.format(
+                    "INSERT INTO alumnos (nombre, apellidos, altura, aula) VALUES ('%s', '%s', %d, %d)", nombre,
+                    apellidos, altura, aula);
             int numFilasAfectadas = st.executeUpdate(consulta);
             System.out.println(numFilasAfectadas);
         } catch (SQLException e) {
@@ -65,29 +59,93 @@ public class ejercicios {
         }
     }
 
-    public static void bajaAlumno(int codigo){
-        
+    public static void altaAsignatura(String nombre) {
+        try (Statement st = conexion.createStatement()) {
+            String consulta = String.format("INSERT INTO asignaturas (nombre) VALUES ('%s')", nombre);
+            int numFilasAfectadas = st.executeUpdate(consulta);
+            System.out.println(numFilasAfectadas);
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+        }
     }
 
-    public static void bajaAsignatura(int codigo){
-
+    public static void bajaAlumno(int codigo) {
+        try (Statement st = conexion.createStatement()) {
+            String consulta = String.format("DELETE FROM alumnos WHERE codigo = %d", codigo);
+            int numFilasAfectadas = st.executeUpdate(consulta);
+            System.out.println(numFilasAfectadas);
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+        }
     }
-    
+
+    public static void bajaAsignatura(int codigo) {
+        try (Statement st = conexion.createStatement()) {
+            String consulta = String.format("DELETE FROM asignaturas WHERE cod = %d", codigo);
+            int numFilasAfectadas = st.executeUpdate(consulta);
+            System.out.println(numFilasAfectadas);
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+        }
+    }
+
+    public static void modificarAlumno(String nombreViejo, String nombreNuevo) {
+        try (Statement st = conexion.createStatement()) {
+            String consulta = String.format("UPDATE alumnos SET nombre = '%s' WHERE nombre = '%s'", nombreNuevo,
+                    nombreViejo);
+            int numFilasAfectadas = st.executeUpdate(consulta);
+            System.out.println(numFilasAfectadas);
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+        }
+    }
+
+    public static void modificarAsignatura(String asignaturaVieja, String asignaturaNueva) {
+        try (Statement st = conexion.createStatement()) {
+            String consulta = String.format("UPDATE asignaturas SET nombre = '%s' WHERE nombre = '%s'", asignaturaNueva,
+                    asignaturaVieja);
+            int numFilasAfectadas = st.executeUpdate(consulta);
+            System.out.println(numFilasAfectadas);
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+        }
+    }
+
+    public static void aulasConAlumnos() {
+        try (Statement st = conexion.createStatement()) {
+            String consulta = "SELECT nombre FROM alumnos; SELECT nombre FROM asignaturas;";
+            ResultSet resultado = st.executeQuery(consulta);
+            while (resultado.next()) {
+                sou
+            }
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+        }
+    }
+
     public static void main(String[] args) throws SQLException {
         abrirConexion("add", "localhost", "root", "");
         System.out.println("-----------EJERCICIO 1----------------");
         consultarDatos("a");
         System.out.println("-----------EJERCICIO 2----------------");
-        altaAlumno("Carlos", "Italiani", 187, 20);
+        // altaAlumno("Carlos", "Italiani", 187, 20);
         System.out.println("-----------EJERCICIO 2.1----------------");
-        altaAsignatura("Acceso a datos");
+        // altaAsignatura("Acceso a datos");
         System.out.println("-----------EJERCICIO 3----------------");
-        bajaAlumno(10);
+        // bajaAlumno(10);
         System.out.println("-----------EJERCICIO 3.1----------------");
-        bajaAsignatura(9);
+        // bajaAsignatura(9);
         System.out.println("-----------EJERCICIO 4----------------");
+        // modificarAlumno("Frank","Franco");
+        System.out.println("-----------EJERCICIO 4.1----------------");
+        // modificarAsignatura("FOL","Empresa");
+        System.out.println("-----------EJERCICIO 5----------------");
 
         cerrarConexion();
     }
-    // Ejercicios: 1.- Nombre y edad >30 años; 2.- Insertar Manuel ; 3.- Actualizar Guaita por Radu 
+    // try (Statement st = conexion.createStatement()){
+
+    // } catch (SQLException e) {
+    // System.out.println("ERROR");
+    // }
 }
