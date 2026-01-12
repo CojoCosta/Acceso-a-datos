c4e01cad8f87a23816f7e32e800a7775f5b90e12
CojoCosta <dcosta1882000@gmail.com>
2026-01-12 11:46:17 +0100

    changes 12/01

---
+++ API/tema5maven/target/tema5maven/Persona.java
@@ -0,0 +1,40 @@
+import java.io.Serializable;
+
+public class Persona implements Serializable{
+    public int id;
+    public String cadena;
+    public boolean casado;
+    public String sexo;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCadena(String cadena) {
+        this.cadena = cadena;
+    }
+
+    public void setCasado(boolean casado) {
+        this.casado = casado;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCadena() {
+        return cadena;
+    }
+
+    public boolean getCasado() {
+        return casado;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+}
